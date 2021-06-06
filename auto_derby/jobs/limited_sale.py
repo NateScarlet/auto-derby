8c38eb6f8c65bfc2eca20b4e48c7b6afa0516aea
NateScarlet <NateScarlet@Gmail.com>
2021-06-06 21:09:21 +0800

    fix(limited-sale): close button click may not work
    fix #28

--- auto_derby/jobs/limited_sale.py
+++ auto_derby/jobs/limited_sale.py
@@ -5,7 +5,7 @@ from .. import action, templates
 
 
 def buy_everything():
-    action.click_image(templates.GO_TO_LIMITED_SALE)
+    action.wait_click_image(templates.GO_TO_LIMITED_SALE)
     action.wait_image(templates.CLOSE_NOW_BUTTON)
     for _, pos in action.match_image_until_disappear(
         templates.EXCHANGE_BUTTON,
@@ -13,14 +13,15 @@ def buy_everything():
     ):
         action.click(pos)
         action.wait_click_image(templates.EXCHANGE_CONFIRM_BUTTON)
-        action.wait_click_image(templates.CLOSE_BUTTON)
+        for _, pos in action.match_image_until_disappear(templates.CLOSE_BUTTON):
+            action.click(pos)
         action.wait_image(templates.CLOSE_NOW_BUTTON)
         with action.recover_cursor():
             action.move(pos)
             action.wheel(-2)
 
-    action.click_image(templates.CLOSE_NOW_BUTTON)
-    action.click_image(templates.GREEN_OK_BUTTON)
+    action.wait_click_image(templates.CLOSE_NOW_BUTTON)
+    action.wait_click_image(templates.GREEN_OK_BUTTON)
     action.wait_image(templates.RETURN_BUTTON)
     for _, pos in action.match_image_until_disappear(templates.RETURN_BUTTON):
         action.click(pos)
