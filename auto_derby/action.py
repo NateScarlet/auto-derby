c6b7141de21435fd681ed40a8c1d146cdb627a29
NateScarlet <NateScarlet@Gmail.com>
2021-06-21 16:21:44 +0800

    fix: mistaken deleted line in action.py
    fix #70

--- auto_derby/action.py
+++ auto_derby/action.py
@@ -38,6 +38,7 @@ def match_image_until_disappear(
         for i in sort(template.match(template.screenshot(max_age=0), *tmpl)):
             count += 1
             yield i
+            break  # actions will make screenshot outdate
         if count == 0:
             break
 
