f6c4300895ffbe0ef44c11ff12cdbd873825dd64
NateScarlet <NateScarlet@Gmail.com>
2021-07-12 21:16:54 +0800

    feat(plugin): add prefer_g1 plugin

---
+++ plugins/prefer_g1.py
@@ -0,0 +1,19 @@
+import auto_derby
+from auto_derby import single_mode
+
+
+class Plugin(auto_derby.Plugin):
+    """Use this after other plugin, increase g1 score if estimate order <= 3.  """
+
+    def install(self) -> None:
+        class Race(auto_derby.config.single_mode_race_class):
+            def score(self, ctx: single_mode.Context) -> float:
+                ret = super().score(ctx)
+                if self.grade == Race.GRADE_G1 and self.estimate_order(ctx) <= 3:
+                    ret += 10
+                return ret
+
+        auto_derby.config.single_mode_race_class = Race
+
+
+auto_derby.plugin.register(__name__, Plugin())
