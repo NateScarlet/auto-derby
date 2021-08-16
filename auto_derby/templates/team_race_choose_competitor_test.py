2f265db6995ebeeb69dc7d92f392f1ffc7eb1ae9
NateScarlet <NateScarlet@Gmail.com>
2021-08-17 01:28:36 +0800

    fix(team-race): update template for adb device
    fix #144

---
+++ auto_derby/templates/team_race_choose_competitor_test.py
@@ -0,0 +1,22 @@
+from auto_derby import mathtools
+from . import _test
+from .. import template, templates
+
+
+_EXPECTED_POS = (240, 154)
+
+
+def test_match():
+    img, rp = _test.use_screenshot("team_race_arena.png")
+    res = tuple(template.match(img, templates.TEAM_RACE_CHOOSE_COMPETITOR))
+    assert len(res) == 1
+    (match1,) = res
+    assert match1[1] == rp.vector2(_EXPECTED_POS, 540)
+
+
+def test_match_issue135():
+    img, rp = _test.use_screenshot("team_race_arena_issue144.png")
+    res = tuple(template.match(img, templates.TEAM_RACE_CHOOSE_COMPETITOR))
+    assert len(res) == 1
+    (match1,) = res
+    assert mathtools.distance(match1[1], rp.vector2(_EXPECTED_POS, 540)) < 8
