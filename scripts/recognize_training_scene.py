0929f348286c1e6545fc443ab157b074d801010a
NateScarlet <NateScarlet@Gmail.com>
2021-08-21 12:47:07 +0800

    chore: update scripts/recognize_training_scene.py

--- scripts/recognize_training_scene.py
+++ scripts/recognize_training_scene.py
@@ -2,6 +2,7 @@
 # pyright: strict
 
 from __future__ import annotations
+import logging
 
 if True:
     import os
@@ -27,13 +28,25 @@ def main():
 
     parser.add_argument("image", default="debug/last_screenshot.png")
     parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--debug-partner", action="store_true")
     args = parser.parse_args()
     if args.debug:
-        os.environ["DEBUG"] = "auto_derby.single_mode.training"
+        os.environ["DEBUG"] = "auto_derby.single_mode.training.training"
+    if args.debug_partner:
+        os.environ["DEBUG"] = "auto_derby.single_mode.training.partner"
+    for i in os.getenv("DEBUG", "").split(","):
+        if not i:
+            continue
+        logging.getLogger(i).setLevel(logging.DEBUG)
     image_path = args.image
     image = imagetools.resize(PIL.Image.open(image_path), width=template.TARGET_WIDTH)
     recognize_training(image)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    logging.basicConfig(
+        format="%(levelname)-6s[%(asctime)s]:%(name)s:%(lineno)d: %(message)s",
+        level=logging.INFO,
+        datefmt="%H:%M:%S",
+    )
     main()
