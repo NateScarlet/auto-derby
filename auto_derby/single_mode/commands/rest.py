f2483d0b1dec03f6515ca41d1ae7d963ecebc2ff
NateScarlet <NateScarlet@Gmail.com>
2021-08-23 00:34:09 +0800

    feat(single-mode): adjust rest score

--- auto_derby/single_mode/commands/rest.py
+++ auto_derby/single_mode/commands/rest.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from ... import action, templates
 from ...scenes.single_mode.command import CommandScene
-from .. import Context
+from .. import Context, Training
 from .command import Command
 from .globals import g
 
@@ -23,21 +23,9 @@ class RestCommand(Command):
 
 
 def default_score(ctx: Context) -> float:
-    ret = 15 + ctx.turn_count() * 10 / 24
-
-    if ctx.vitality > 0.8:
-        ret *= 0.3
-    elif ctx.vitality > 0.5:
-        ret *= 0.5
-    if ctx.turn_count() >= ctx.total_turn_count() - 2:
-        ret *= 0.1
-    if ctx.date[1:] in ((6, 1),) and ctx.vitality < 0.8:
-        ret += 10
-    if ctx.date[1:] in ((6, 2),) and ctx.vitality < 0.9:
-        ret += 20
-    if ctx.date in ((4, 0, 0)):
-        ret -= 20
-    return ret
+    t = Training.new()
+    t.vitality = 50 / ctx.total_vitality
+    return t.score(ctx)
 
 
 g.rest_score = default_score
