dffca2ef62f2a077e68c442c1b7cd0e3d6821d38
NateScarlet <NateScarlet@Gmail.com>
2021-08-24 20:35:38 +0800

    chore: add scripts/recognize_command_scene.py

---
+++ scripts/recognize_command_scene.py
@@ -0,0 +1,42 @@
+# -*- coding=UTF-8 -*-
+# pyright: strict
+
+from __future__ import annotations
+import logging
+
+if True:
+    import os
+    import sys
+
+    sys.path.insert(0, os.path.join(__file__, "../.."))
+
+
+import argparse
+
+import PIL.Image
+from auto_derby import imagetools, single_mode, template
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("image", default="debug/last_screenshot.png")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    if args.debug:
+        logging.getLogger("auto_derby").setLevel(logging.DEBUG)
+    image_path = args.image
+    image = imagetools.resize(PIL.Image.open(image_path), width=template.TARGET_WIDTH)
+    ctx = single_mode.Context()
+    ctx.update_by_command_scene(image)
+    print(ctx)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(levelname)-6s[%(asctime)s]:%(name)s:%(lineno)d: %(message)s",
+        level=logging.INFO,
+        datefmt="%H:%M:%S",
+    )
+    main()
