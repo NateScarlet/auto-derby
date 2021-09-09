959e0e34310f2affcb6abf878abbb0aeff3e87d9
NateScarlet <NateScarlet@Gmail.com>
2021-09-10 00:07:09 +0800

    test: add go out test data for 樫本理子

--- auto_derby/single_mode/go_out_test.py
+++ auto_derby/single_mode/go_out_test.py
@@ -1,14 +1,22 @@
+from typing import Text
 from .go_out import Option
 
 from .. import _test
+import pytest
 
 
-def test_from_menu():
-    img, _ = _test.use_screenshot("single_mode/go_out_menu.png")
+@pytest.mark.parametrize(
+    "name",
+    tuple(
+        i.stem for i in ((_test.DATA_PATH / "single_mode").glob("go_out_menu_*.png"))
+    ),
+)
+def test_from_menu(name: Text):
+    img, _ = _test.use_screenshot(f"single_mode/{name}.png")
     res = Option.from_menu(img)
 
-    support_card, character = sorted(res, key=lambda x: x.position[1])
-    _test.snapshot_match([support_card, character])
-    assert support_card.type == Option.TYPE_SUPPORT, support_card.type
-    assert support_card.current_event_count == 0, support_card.current_event_count
-    assert character.type == Option.TYPE_MAIN, character.type
+    res = sorted(res, key=lambda x: x.position[1])
+    _test.snapshot_match(
+        res,
+        name=name,
+    )
