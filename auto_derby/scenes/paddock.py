0dc8cc036e88af5d80f615a943e001cbcffbb5cf
Huang Yue <cna.bld@gmail.com>
2021-09-09 22:45:14 +0800

    fix: wrong button pos in PaddockScene.choose_runing_style (#187)

--- auto_derby/scenes/paddock.py
+++ auto_derby/scenes/paddock.py
@@ -23,10 +23,10 @@ class PaddockScene(Scene):
     def choose_runing_style(self, style: RuningStyle):
         rp = action.resize_proxy()
         button_pos = {
-            RuningStyle.LEAD: rp.vector2((60, 500), 466),
-            RuningStyle.HEAD: rp.vector2((160, 500), 466),
-            RuningStyle.MIDDLE: rp.vector2((260, 500), 466),
-            RuningStyle.LAST: rp.vector2((360, 500), 466),
+            RuningStyle.LEAD: rp.vector2((360, 500), 466),
+            RuningStyle.HEAD: rp.vector2((260, 500), 466),
+            RuningStyle.MIDDLE: rp.vector2((160, 500), 466),
+            RuningStyle.LAST: rp.vector2((60, 500), 466),
         }[style]
         action.tap_image(templates.RACE_RUNNING_STYLE_CHANGE_BUTTON)
         _, pos = action.wait_image(templates.RACE_CONFIRM_BUTTON)
