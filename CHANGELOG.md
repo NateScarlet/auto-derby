9ed4ddfe0e8cb05b2667bc534363f6e872363f9d
NateScarlet <NateScarlet@Gmail.com>
2021-12-12 20:56:58 +0800

    chore(release): 1.12.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+## [1.12.2](https://github.com/NateScarlet/auto-derby/compare/v1.12.1...v1.12.2) (2021-12-12)
+
+### Bug Fixes
+
+- **single-mode:** can not recgonize ura race ([269ea03](https://github.com/NateScarlet/auto-derby/commit/269ea03e80e4e01ef4238e776aea6c20189028b3)), closes [#217](https://github.com/NateScarlet/auto-derby/issues/217)
+
 ## [1.12.1](https://github.com/NateScarlet/auto-derby/compare/v1.12.0...v1.12.1) (2021-12-11)
 
 ### Bug Fixes
--- auto_derby/__version__.py
+++ auto_derby/__version__.py
@@ -9,12 +9,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from datetime import datetime
 
-VERSION = "1.12.1"
-RELEASE_DATE = datetime.fromtimestamp(1639237050)
-LAST_GIT_COMMIT_DESCRIBE = "v1.12.0-2-g1c599a1"
-LAST_GIT_COMMIT_HASH = "1c599a14942002acb51df1e8cf89394c15b5ad1d"
+VERSION = "1.12.2"
+RELEASE_DATE = datetime.fromtimestamp(1639313815)
+LAST_GIT_COMMIT_DESCRIBE = "v1.12.1-2-g269ea03"
+LAST_GIT_COMMIT_HASH = "269ea03e80e4e01ef4238e776aea6c20189028b3"
 LAST_GIT_COMMIT_AUTHOR_NAME = "NateScarlet"
 LAST_GIT_COMMIT_AUTHOR_EMAIL = "NateScarlet@Gmail.com"
-LAST_GIT_COMMIT_AUTHOR_DATE = datetime.fromtimestamp(1639236855)
-LAST_GIT_COMMIT_SUBJECT = "fix(single-race): can not regonize race menu item with reward"
-LAST_GIT_COMMIT_BODY = "fix #216"
+LAST_GIT_COMMIT_AUTHOR_DATE = datetime.fromtimestamp(1639313628)
+LAST_GIT_COMMIT_SUBJECT = "fix(single-mode): can not recgonize ura race"
+LAST_GIT_COMMIT_BODY = "fix #217"
