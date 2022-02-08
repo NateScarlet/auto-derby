fe4e12bab52ae0e5d7eb9294ab4522385fd93219
NateScarlet <NateScarlet@Gmail.com>
2022-02-09 06:42:15 +0800

    feat(plugin): add `example_tamamo_cross` plugin

--- auto_derby/single_mode/race/race_result.py
+++ auto_derby/single_mode/race/race_result.py
@@ -7,7 +7,7 @@ import datetime
 import json
 import logging
 import os
-from typing import Any, Dict, Iterator, Text
+from typing import Any, Dict, Iterator, List, Optional, Text
 
 from ..context import Context
 from .globals import g
@@ -74,6 +74,48 @@ class RaceResult:
             os.makedirs(os.path.dirname(p))
             _do()
 
+    def is_current(self, ctx: Context) -> Optional[bool]:
+        """
+        Whether is race result of current game.
+        Returns None when result is unknown.
+        """
+
+        if self.ctx.date > ctx.date:
+            return False
+
+        if (
+            self.ctx.scenario != Context.SCENARIO_UNKNOWN
+            and ctx.scenario != Context.SCENARIO_UNKNOWN
+            and self.ctx.scenario != ctx.scenario
+        ):
+            return
+
+        # fan count never reduce
+        if self.ctx.fan_count > ctx.fan_count:
+            return
+
+        # distance status never reduce
+        if (
+            self.ctx.sprint > ctx.sprint
+            or self.ctx.intermediate > ctx.intermediate
+            or self.ctx.mile > ctx.mile
+            or self.ctx.long > ctx.long
+        ):
+            return False
+
+        # ground status never reduce
+        if self.ctx.dart > ctx.dart or self.ctx.turf > ctx.turf:
+            return False
+
+        # style status never reduce
+        if (
+            self.ctx.lead > ctx.lead
+            or self.ctx.head > ctx.head
+            or self.ctx.middle > ctx.middle
+            or self.ctx.last > ctx.last
+        ):
+            return False
+
 
 def iterate() -> Iterator[RaceResult]:
     p = g.result_path
@@ -101,3 +143,17 @@ def prune(time_lt: datetime.datetime) -> None:
         pass
     os.rename(p, p + "~")
     os.rename(tmp_path, p)
+
+
+def iterate_current(ctx: Context) -> Iterator[RaceResult]:
+    buf: List[RaceResult] = []
+    for i in iterate():
+        if i.is_current(ctx) == False:
+            buf.clear()
+            continue
+
+        if buf and buf[-1].ctx.date > i.ctx.date:
+            buf.clear()
+        buf.append(i)
+    for i in buf:
+        yield i
---
+++ plugins/example_tamamo_cross.py
@@ -0,0 +1,71 @@
+# -*- coding=UTF-8 -*-
+
+import auto_derby
+from auto_derby import mathtools
+
+from typing import Tuple
+
+
+import logging
+from auto_derby.single_mode.context import Context
+
+from auto_derby.single_mode.race.race import Race
+from auto_derby.single_mode.race import race_result
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class _g:
+    cached_is_year2_target_pending: Tuple[Tuple[int, int, int], bool] = (
+        (0, 0, 0),
+        False,
+    )
+
+
+def _raw_is_year2_target_pending(ctx: Context) -> bool:
+    if ctx.date < (2, 1, 1) or ctx.date > (3, 1, 1):
+        return False
+
+    winning_count = 0
+    for i in race_result.iterate_current(ctx):
+        if i.race.grade <= Race.GRADE_G3 and i.order <= 3:
+            winning_count += 1
+        if winning_count >= 4:
+            return False
+    _LOGGER.info("year2 target pending: %d/4", winning_count)
+    return True
+
+
+def _is_year2_target_pending(ctx: Context) -> bool:
+    if ctx.date != _g.cached_is_year2_target_pending[0]:
+        _g.cached_is_year2_target_pending = (
+            ctx.date,
+            _raw_is_year2_target_pending(ctx),
+        )
+    return _g.cached_is_year2_target_pending[1]
+
+
+class Plugin(auto_derby.Plugin):
+    def install(self) -> None:
+        class Race(auto_derby.config.single_mode_race_class):
+            def score(self, ctx: Context) -> float:
+                ret = super().score(ctx)
+                if (
+                    self.grade <= Race.GRADE_G3
+                    and _is_year2_target_pending(ctx)
+                    and self.estimate_order(ctx) <= 5
+                ):
+                    ret += mathtools.interpolate(
+                        ctx.turn_count(),
+                        (
+                            (24, 10.0),
+                            (48, 100.0),
+                        ),
+                    )
+
+                return ret
+
+        auto_derby.config.single_mode_race_class = Race
+
+
+auto_derby.plugin.register(__name__, Plugin())
