75c751c1f0fb7084bd5568589ac81a3c4f149a20
NateScarlet <NateScarlet@Gmail.com>
2022-04-14 02:44:21 +0800

    feat: extract character data

---
+++ auto_derby/character.py
@@ -0,0 +1,119 @@
+# -*- coding=UTF-8 -*-
+# pyright: strict
+
+from __future__ import annotations
+
+import enum
+import json
+from typing import Any, Dict, Iterator, Protocol, Text, Tuple
+
+from . import data, filetools
+
+
+class Gender(enum.Enum):
+    STALLION = 1
+    MARE = 2
+
+
+class Repository(Protocol):
+    def save(self, c: Character) -> None:
+        ...
+
+    def find(self, *, name: Text = "", id: int = 0) -> Iterator[Character]:
+        """
+        >>> r = Character.repository
+
+        Get by id:
+        >>> print(next(r.find(id=1001)))
+        Character<スペシャルウィーク#1001>
+
+        Get by name:
+        >>> print(next(r.find(name="スペシャルウィーク")))
+        Character<スペシャルウィーク#1001>
+
+        Iterate all:
+        >>> for i in r.find():
+        ...     pass
+        """
+        ...
+
+
+class Character:
+    repository: Repository
+
+    def __init__(
+        self,
+        id: int,
+        name: Text,
+        real_name: Text,
+        katakana: Text,
+        voice: Text,
+        birthday: Tuple[int, int, int],
+        gender: Gender,
+    ) -> None:
+        self.id = id
+        self.name = name
+        self.real_name = real_name
+        self.katakana = katakana
+        self.voice = voice
+        self.birthday = birthday
+        self.gender = gender
+
+    def __str__(self):
+        return f"Character<{self.name}#{self.id}>"
+
+
+class JSONLRepository(Repository):
+    def __init__(self, path: Text) -> None:
+        self.path = path
+
+    def _to_po(self, c: Character) -> Dict[Text, Any]:
+        return {
+            "id": c.id,
+            "name": c.name,
+            "realName": c.real_name,
+            "katakana": c.katakana,
+            "voice": c.voice,
+            "birthday": c.birthday,
+            "gender": c.gender.name,
+        }
+
+    def _from_po(self, data: Dict[Text, Any]) -> Character:
+        return Character(
+            data["id"],
+            data["name"],
+            data["realName"],
+            data["katakana"],
+            data["voice"],
+            tuple(data["birthday"]),
+            Gender[data["gender"]],
+        )
+
+    def save(self, c: Character) -> None:
+        # rewrite whole file to save single item is expensive, but who cares?
+        data = sorted(
+            [*(i for i in self.find() if i.id != c.id), c],
+            key=lambda x: x.id,
+        )
+        with filetools.atomic_save_path(self.path) as save_path, open(
+            save_path, "w", encoding="utf-8"
+        ) as f:
+            for i in data:
+                json.dump(self._to_po(i), f, ensure_ascii=False)
+                f.write("\n")
+
+    def find(self, *, name: Text = "", id: int = 0) -> Iterator[Character]:
+        try:
+            with open(self.path, "r", encoding="utf-8") as f:
+                for line in f:
+                    c = self._from_po(json.loads(line))
+                    if name and c.name != name:
+                        continue
+                    if id and c.id != id:
+                        continue
+                    yield c
+        except FileNotFoundError:
+            return
+
+
+Character.repository = JSONLRepository(data.path("characters.jsonl"))
---
+++ scripts/extract_characters.py
@@ -0,0 +1,67 @@
+# -*- coding=UTF-8 -*-
+# pyright: strict
+# spell-checker: words chara
+
+
+if True:
+    import os
+    import sys
+
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+import argparse
+import contextlib
+import os
+import sqlite3
+from typing import Iterator, Text
+
+from auto_derby.character import Character, Gender
+
+
+def _read_master_mdb(path: Text) -> Iterator[Character]:
+
+    db = sqlite3.connect(path)
+    cur = db.execute(
+        """
+SELECT
+  t1.id,
+  t2.text,
+  t3.text,
+  t4.text,
+  t5.text,
+  t1.birth_year,
+  t1.birth_month,
+  t1.birth_day,
+  t1.sex
+  FROM chara_data AS t1
+  LEFT JOIN text_data AS t2 ON t2.category = 6 AND t2."index" = t1.id
+  LEFT JOIN text_data AS t3 ON t3.category = 170 AND t3."index" = t1.id
+  LEFT JOIN text_data AS t4 ON t4.category = 182 AND t4."index" = t1.id
+  LEFT JOIN text_data AS t5 ON t5.category = 7 AND t5."index" = t1.id
+;
+    """
+    )
+
+    with contextlib.closing(cur):
+        for i in cur:
+            yield Character(i[0], i[1], i[2], i[3], i[4], tuple(i[5:8]), Gender(i[8]))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path",
+        nargs="?",
+        default=os.path.expandvars(
+            "${LocalAppData}Low/cygames/umamusume/master/master.mdb"
+        ),
+    )
+    args = parser.parse_args()
+    path: Text = args.path
+
+    for char in _read_master_mdb(path):
+        Character.repository.save(char)
+
+
+if __name__ == "__main__":
+    main()
