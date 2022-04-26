0e6a85ada5912693f18b12ee38759822aae18c4f
NateScarlet <NateScarlet@Gmail.com>
2022-04-27 00:41:44 +0800

    feat(plugin): auto_crane: adjust press duration

--- plugins/auto_crane.py
+++ plugins/auto_crane.py
@@ -5,7 +5,7 @@ from auto_derby import action, single_mode, templates
 
 
 def play(ctx: single_mode.Context):
-    for duration in (2.2, 1.8, 1.2):
+    for duration in (2, 1.6, 1):
         _, pos = action.wait_image(templates.SINGLE_MODE_CRANE_GAME_BUTTON)
         time.sleep(3)
         _, pos = action.wait_image(templates.SINGLE_MODE_CRANE_GAME_BUTTON)
