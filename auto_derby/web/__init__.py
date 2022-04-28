682eeb60e2015db5afa893446b6ce757189249b3
NateScarlet <NateScarlet@Gmail.com>
2022-04-29 01:59:13 +0800

    wo[

--- auto_derby/web/__init__.py
+++ auto_derby/web/__init__.py
@@ -1,5 +1,5 @@
 from ._prompt import prompt
-from .webview import Webview, NoOpWebview, DefaultWebview
+from .webview import Webview, NoOpWebview
 from .middleware import File, Route, Dir, Blob
 from . import middleware, page
 from ._stream import stream
