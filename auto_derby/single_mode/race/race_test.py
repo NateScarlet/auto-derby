92759f6e43c5b8532fbb7dac21bc5d543eaf6513
NateScarlet <NateScarlet@Gmail.com>
2022-08-21 14:14:41 +0800

    test(single-mode): add race menu test case

--- auto_derby/single_mode/race/race_test.py
+++ auto_derby/single_mode/race/race_test.py
@@ -322,6 +322,15 @@ def test_find_by_race_menu_image_12():
     _test.snapshot_match(res)
 
 
+def test_find_by_race_menu_image_13():
+    ctx = Context.new()
+    ctx.date = (3, 5, 1)
+    ctx.scenario = ctx.SCENARIO_CLIMAX
+    img, _ = _test.use_screenshot("single_mode/race_menu_13.png")
+    res = sorted(race.find_by_race_menu_image(ctx, img), key=lambda x: x[1][1])
+    _test.snapshot_match(res)
+
+
 def test_find_by_race_menu_image_issue112():
     ctx = Context.new()
     ctx.date = (1, 12, 1)
